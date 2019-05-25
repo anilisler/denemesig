99f702d35f385375c7c61921b87e3fdea7207c3e
Anil Isler <dev@anilisler.com>
2019-05-25 19:43:52 +0300

    online users count showing. online users connection ids showing. edited html for better understanding. message text box emptying after sending a new message.

--- denemesig/ChatHub.cs
+++ denemesig/ChatHub.cs
@@ -1,13 +1,46 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace denemesig
 {
     public class ChatHub : Hub
     {
+        // how many users online
+        private static int _userCount = 0;
+
+        //online users connection IDs
+        private static List<string> _userList = new List<string>();
+
+        //send message to all connected clients
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        //works after client connects to hub
+        public override Task OnConnectedAsync()
+        {
+            _userCount++;
+            _userList.Add(Context.ConnectionId);
+            base.OnConnectedAsync();
+            this.Clients.All.SendAsync("updateCount", _userCount);
+            this.Clients.All.SendAsync("updateUserList", _userList);
+            return Task.CompletedTask;
+        }
+
+        //works after client disconnects from hub
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _userCount--;
+            _userList.Remove(Context.ConnectionId);
+            base.OnDisconnectedAsync(exception);
+            this.Clients.All.SendAsync("updateCount", _userCount);
+            this.Clients.All.SendAsync("updateUserList", _userList);
+            return Task.CompletedTask;
+        }
+
+
     }
 }
--- denemesig/wwwroot/js/chat.js
+++ denemesig/wwwroot/js/chat.js
@@ -5,25 +5,67 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
+//Start connecting to hub
+connection.start().then(function () {
+    console.log("connected to hub successfully!");
+    document.getElementById("sendButton").disabled = false;
+    updateOnlineUserCount(connection);
+    updateOnlineUserList(connection);
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+//calling client method "ReceiveMessage" from hub
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
+    //add new message to list
     document.getElementById("messagesList").appendChild(li);
-});
-
-connection.start().then(function(){
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+    document.getElementById("messageInput").value = "";
+    //calling hub method "SendMessage" from client
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+function updateOnlineUserCount(connection) {
+    var messageCallback = function (message) {
+        console.log('message' + message);
+        if (!message) return;
+        var userCountSpan = document.getElementById('onlineUsersCount');
+        userCountSpan.innerText = "Online users: " + message;
+    };
+    //calling client method "updateCount" from hub
+    connection.on("updateCount", messageCallback);
+    connection.onclose(onConnectionError);
+}
+
+function updateOnlineUserList(connection) {
+    var funcCallback = function (userList) {
+        console.log('userList' + userList);
+        if (!userList) return;
+        document.getElementById("onlineUsersList").innerHTML = "";
+        userList.forEach(element => {
+            var li = document.createElement("li");
+            li.textContent = element;
+            document.getElementById("onlineUsersList").appendChild(li);
+        });
+    };
+    //calling client method "updateUserList" from hub
+    connection.on("updateUserList", funcCallback);
+    connection.onclose(onConnectionError);
+}
+
+function onConnectionError(error) {
+    if (error && error.message) {
+        console.error(error.message);
+    }
+}
